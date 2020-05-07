f7dddfe8c5d80791248c127f31afa8b4064f26b4
Delbert <delbertyap27@gmail.com>
2020-05-07 18:55:04 +0800

    Done

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,7 +2,12 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoView rabbidVideo = findViewById(R.id.rabbidVideo);
+        rabbidVideo.setVideoURI(
+                Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid));
+        rabbidVideo.start();
     }
 }
